f6fb8f99a4f3899d093316a425b42b2aa0fa0a3f
shoma115 <125989314+shoma115@users.noreply.github.com>
2023-06-08 19:22:40 +0900

    Add files via upload

---
+++ migrations/2023_06_04_155342_create_classdata_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('classdata', function (Blueprint $table) {
+            $table->id();
+            $table->string('class_name');
+            $table->string('class_week_day');
+            $table->integer('class_time');
+            $table->string('class_place');
+            $table->integer('amout_credit');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('data');
+    }
+};
