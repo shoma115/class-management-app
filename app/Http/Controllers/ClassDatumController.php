718de119c829bc08285786819aad30466981b316
shoma115 <xiangzhendaogen8@gmail.com>
2023-10-14 04:17:17 +0900

    時間割作成のアルゴリズムを変更

--- app/Http/Controllers/ClassDatumController.php
+++ app/Http/Controllers/ClassDatumController.php
@@ -17,11 +17,36 @@ class ClassDatumController extends Controller
 
     // 時間割のreadページを表示するアクション
     public function read() {
-        
+        //１限目～何限目まであるかの数値
+        $periods = [1, 2, 3, 4, 5];
+        $weeks = ['月', '火', '水', '木', '金', '土'];
+        $week_to_number = [
+                            '月曜' => 0*count($periods), 
+                            '火曜' => 1*count($periods), 
+                            '水曜' => 2*count($periods), 
+                            '木曜' => 3*count($periods), 
+                            '金曜' => 4*count($periods), 
+                            '土曜' => 5*count($periods) 
+                        ];
         $classdata = Auth::user()->class_data;
-        return view('classdatum.read', compact('classdata'));
+        $timetable = [];
+        
+        foreach($weeks as $week) {
+            foreach($periods as $period) {
+                $timetable[count($timetable)] = ["class_name" => "+", "class_place" => "", "route" => route('resource.select', ['week' => $week . '曜', 'time' => $period])];
+            }
+        }
+        foreach($classdata as $classdatum) {
+            $array_key = $week_to_number[$classdatum->class_week_day] + $classdatum->class_time - 1;
+            $classdatum["route"] = route('classdata.show', $classdatum);
+            $timetable[$array_key] = $classdatum;
+            
+        }
+
+        return view('classdatum.read', compact('classdata', 'periods', 'weeks', 'timetable'));
         
     }
+
     // 授業の詳細を表示showアクション
     public function show(ClassDatum $classdatum) {
         return view('classdatum.show', compact('classdatum'));
--- resources/views/classdatum/read.blade.php
+++ resources/views/classdatum/read.blade.php
@@ -15,158 +15,28 @@
             </nav>
 
             <article class="pb-2">
-                
-                         
-                <table class="table p-0 border">
-                    <tr>
-                        <td style="width: 2%" class="bg-secondary text-white"></td>
-                        <th id="mon" style="width: 15%" class="p-1 bg-secondary text-white border th">月</th>
-                        <th id="tue" style="width: 15%" class="p-1 bg-secondary text-white border th">火</th>
-                        <th id="wed" style="width: 15%" class="p-1 bg-secondary text-white border th">水</th>
-                        <th id="thu" style="width: 15%" class="p-1 bg-secondary text-white border th">木</th>
-                        <th id="fri" style="width: 15%" class="p-1 bg-secondary text-white border th">金</th>
-                        <th id="sat" style="width: 15%" class="p-1 bg-secondary text-white border th">土</th>
-                    </tr>
-                    <tr>
-                        <th class="p-0 my-auto time bg-secondary-subtle border-top border-dark th" style="height: 120px">一限目</th>
-                        <td id="mon1" class="p-0 border border-dark"></td>
-                        <td id="tue1" class="p-0 border border-dark"></td>
-                        <td id="wed1" class="p-0 border border-dark"></td>
-                        <td id="thu1" class="p-0 border border-dark"></td>
-                        <td id="fri1" class="p-0 border border-dark"></td>
-                        <td id="sat1" class="p-0 border border-dark"></td>
-                    </tr>
-                    <tr>
-                        <th class="p-0 time bg-secondary-subtle border-top border-dark th" style="height: 120px">二限目</th>
-                        <td id="mon2" class="p-0 border border-dark"></td>
-                        <td id="tue2" class="p-0 border border-dark"></td>
-                        <td id="wed2" class="p-0 border border-dark"></td>
-                        <td id="thu2" class="p-0 border border-dark"></td>
-                        <td id="fri2" class="p-0 border border-dark"></td>
-                        <td id="sat2" class="p-0 border border-dark"></td>
-                       
-                    </tr>
-                    <tr>
-                        <th class="p-0 time bg-secondary-subtle border-top border-dark th" style="height: 120px">三限目</th>
-                        <td id="mon3" class="p-0 border border-dark"></td>
-                        <td id="tue3" class="p-0 border border-dark"></td>
-                        <td id="wed3" class="p-0 border border-dark"></td>
-                        <td id="thu3" class="p-0 border border-dark"></td>
-                        <td id="fri3" class="p-0 border border-dark"></td>
-                        <td id="sat3" class="p-0 border border-dark"></td>
-                      
-                    </tr>
-                    <tr>
-                        <th class="p-0 time bg-secondary-subtle border-top border-dark th" style="height: 120px">四限目</th>
-                        <td id="mon4" class="p-0 border border-dark"></td>
-                        <td id="tue4" class="p-0 border border-dark"></td>
-                        <td id="wed4" class="p-0 border border-dark"></td>
-                        <td id="thu4" class="p-0 border border-dark"></td>
-                        <td id="fri4" class="p-0 border border-dark"></td>
-                        <td id="sat4" class="p-0 border border-dark"></td>
-                    </tr>
-                    <tr>
-                        <th class="p-0 time bg-secondary-subtle border-top border-dark th" style="height: 120px">五限目</th>
-                        <td id="mon5" class="p-0 border border-dark"></td>
-                        <td id="tue5" class="p-0 border border-dark"></td>
-                        <td id="wed5" class="p-0 border border-dark"></td>
-                        <td id="thu5" class="p-0 border border-dark"></td>
-                        <td id="fri5" class="p-0 border border-dark"></td>
-                        <td id="sat5" class="p-0 border border-dark"></td>
-                    </tr>
-                    @php
-                        $weekday = array("月曜" => "mon", 
-                                     "火曜" => "tue",
-                                     "水曜" => "wed",
-                                     "木曜" => "thu",
-                                     "金曜" => "fri",
-                                     "土曜" => "sat",
-                                    );
-                    @endphp
-
-                    @foreach($classdata as $classdatum)
-                    @include('modals.modals_classdata.delete_classdata')
-                    @include('modals.modals_classdata.add_task')
-                    @include('modals.modals_classdata.delete_task')
-                    <!-- mon1,thu2などの変数を指定。これでJSを取得する -->
-                    <?php  
-                        $class_week_day = $weekday[$classdatum->class_week_day];
-            $class_week_day . $classdatum->class_time;                         
-                    ?>
-                    <script>
-                        var classaaaa = document.getElementById("classdata");
-
-                        //曜日と時限で授業データを表示する場所を決めるために使う変数を用意 
-                        var classKadai = '';
-                        // 授業の情報をまとめて表示するHTML要素を作成するための変数を用意
-                        var classDetail = '';
-                        // 授業の名前を表示するHTML要素を作成するための変数
-                        var className = '';
-                        // 授業の場所…以下同文
-                        var classPlace = '' ;                        
-
-                         //選択した曜日、時限によって取得するHTML要素を変えている。上のテーブルでid=mon1とかめっちゃ書いてたやつから選んでくる
-                        classKadai = document.getElementById('{{ $class_week_day }}{{ $classdatum->class_time}}');
-
-                        // 授業の詳細を表示するためのHTML要素を作成
-                        classDetail = document.createElement('a');
-                        classDetail.href = "{{ route('classdata.show', $classdatum)}}";
-                        
-                      
-
-                        // classDetailの中で授業名を表示するために作成
-                        // 授業名を押すと課題を追加等のメニューをドロップダウンで表示する
-                        className = document.createElement("div");
-                        classPlace = document.createElement("div")
-                        
-                        classDetail.appendChild(className);
-                        classDetail.appendChild(classPlace);
-                        // 授業名・場所を各要素に文として差し込む
-                        className.innerHTML = '{{ $classdatum->class_name }}';
-                        classPlace.innerHTML = '{{ $classdatum->class_place }}';
-
-                        // 授業の詳細を曜日と時限で取得したＨＴＭＬ要素の子要素に追加する
-                        classKadai.appendChild(classDetail);
-                        classKadai.setAttribute("class", "bg-primary-subtle p-0 border border-dark");
-                                                
-                    </script>
+                <div class="grid">
+                    <div class="item p-1 bg-secondary text-white border th"></div>
+                    @foreach($weeks as $week)
+                        <div class="item p-1 bg-secondary text-white border th">{{$week}}</div>
                     @endforeach
+                </div>
 
-                    <script>
-                        let WAT = '';
-                        let addClass = '';
-                    </script>
-
-                        <?php
-                        $weekAndTimes = ['mon1', 'mon2', 'mon3', 'mon4', 'mon5', 'tue1', 'tue2', 'tue3', 'tue4', 'tue5', 'wed1', 'wed2', 'wed3', 'wed4', 'wed5', 'thu1', 'thu2', 'thu3', 'thu4', 'thu5', 'fri1', 'fri2', 'fri3', 'fri4', 'fri5', 'sat1', 'sat2', 'sat3', 'sat4', 'sat5'];
-                        $weekDay = ['月曜', '火曜', '水曜', '木曜', '金曜', '土曜'];
-                        $classTime = [1, 2, 3, 4, 5];
-                        $weekDayCount = 0;
-                        $classTimeCount = 0;
-                        ?>
-                        
-                        @foreach($weekAndTimes as $weekAndTime)
-                            <script>                                
-                                if({{$weekAndTime}}.childElementCount === 0) {               
-                                        addClass = document.createElement('a');
-                                        addClass.textContent = '+';
-                                        addClass.setAttribute("class", "add-class fs-4 text-decoration-none d-flex align-items-center justify-content-center text-black-50");
-                                        addClass.setAttribute("style", "height: 120px");
-                                        addClass.href = '{{ route('resource.select', ['week' => $weekDay[$weekDayCount], 'time' => $classTime[$classTimeCount]]) }}'
-                                        {{$weekAndTime}}.appendChild(addClass);       
-                                }
-                            </script>
-                            <?php
-                            $classTimeCount++;
-                            if( $classTimeCount % 5 === 0) {
-                                $classTimeCount = 0;
-                                $weekDayCount++;
-                            }
-                            ?>
+                <div class="grid2">
+                    <div class="grid4">
+                        @foreach($periods as $period)
+                            <div class="p-0 bg-secondary-subtle border-top border-dark th">{{ $period }}</div>
                         @endforeach
-                                                    
-                   
-                </table>
-              
+                    </div>
+
+                    <div class="grid3">
+                        @foreach($timetable as $timetable_cell)
+                                    <a class="cell p-1 th" href="{{ $timetable_cell['route'] }}">
+                                        <div class="name">{{ $timetable_cell['class_name'] }}</div><br>
+                                        <div class="place">{{ $timetable_cell['class_place'] }}</div>
+                                    </a>                            
+                        @endforeach
+                    </div>
+                </div>
             </article>
         @endsection
